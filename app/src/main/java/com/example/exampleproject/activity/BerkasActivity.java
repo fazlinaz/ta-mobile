6c7934dfe884c2f18b03ebdc37cabdff4006cfb4
fazlinaz <fazlinazakaria2001@gmail.com>
2023-03-09 19:06:36 +0800

    profile

--- app/src/main/java/com/example/exampleproject/activity/BerkasActivity.java
+++ app/src/main/java/com/example/exampleproject/activity/BerkasActivity.java
@@ -3,17 +3,32 @@ package com.example.exampleproject.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.exampleproject.R;
 
 public class BerkasActivity extends AppCompatActivity {
 
-    AppCompatButton btn
+    AppCompatButton simpan;
+    EditText nama, nik, alamat, no_hp, jenis_kelamin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_berkas);
+
+        simpan = findViewById(R.id.simpan);
+
+        simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent simpan = new Intent(BerkasActivity.this,BerkasActivity1.class);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/exampleproject/activity/ProfileActivity1.java
@@ -0,0 +1,14 @@
+package com.example.exampleproject.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile1);
+    }
+}
